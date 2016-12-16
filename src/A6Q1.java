3ddd41e39711bb309825b273cde6c2ae6a956a70
YBWS <b.yang@sjasd.ca>
2016-12-16 10:27:52 -0600

    A6Q1

---
+++ src/A6Q1.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+
+/**
+ * @author b.yang
+ *
+ */
+public class A6Q1 {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		diaplayName();
+		displayBirthday();
+		displayFact();
+	}
+		
+		public static void diaplayName(){
+			System.out.println("My name: Bowen Yang");
+		}
+		
+		public static void displayBirthday(){
+			System.out.println("My brithday:June 9");
+		}
+		
+		public static void displayFact(){
+			System.out.println("The most insteresting thing about me is go to the other side of the world by myself ");
+		}
+		
+
+}
+
+
